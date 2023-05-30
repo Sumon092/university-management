e79836809f25eb1d8c4cba187ac66fa9b981bc4c
Sumon <m.sumon@outlook.com>
2023-05-31 01:44:28 +0600

    controller and user post route added

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,19 @@
-import express, { Application, Request, Response } from 'express'
+import express, { Application, Response, Request } from 'express'
 import cors from 'cors'
+import userRouter from './app/modules/users/users.route'
 const app: Application = express()
 
 app.use(cors())
+
+//application routes
+app.use('/api/v1/user/', userRouter)
 
 //parser
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Hello World!')
-  console.log(req)
+app.get('/', async (req: Request, res: Response) => {
+  res.send('University app is running !')
 })
 
 export default app

2834c10f7c71f6f91e0bdc0ba45b5940a164b0a6
Sumon <m.sumon@outlook.com>
2023-06-09 09:59:51 +0600

    logger completed

--- src/shared/logger.ts
+++ src/shared/logger.ts
@@ -1,5 +1,6 @@
 import { createLogger, format, transports } from 'winston'
-const { combine, timestamp, label, printf, prettyPrint } = format
+const { combine, timestamp, label, printf } = format
+import DailyRotateFile from 'winston-daily-rotate-file'
 
 import path from 'path'
 
@@ -8,17 +9,26 @@ const myFormat = printf(({ level, message, label, timestamp }) => {
   const hour = date.getHours()
   const min = date.getMinutes()
   const sec = date.getSeconds()
-  return ` [${date} ${hour}:${min}:${sec} ${label}] ${level}: ${message}  `
+  return `${date} ${hour}:${min}:${sec} [${label}] ${level}: ${message}  `
 })
 
 const logger = createLogger({
   level: 'info',
-  format: combine(label({ label: 'PH' }), timestamp(), myFormat, prettyPrint()),
+  format: combine(label({ label: 'PH' }), timestamp(), myFormat),
   transports: [
     new transports.Console(),
-    new transports.File({
-      filename: path.join(process.cwd(), 'logs', 'winston', 'success.log'),
-      level: 'info',
+    new DailyRotateFile({
+      filename: path.join(
+        process.cwd(),
+        'logs',
+        'winston',
+        'successes',
+        'UM-%DATE%-success.log.log'
+      ),
+      datePattern: 'DD-MM-YYYY-HH',
+      zippedArchive: true,
+      maxSize: '20m',
+      maxFiles: '14d',
     }),
   ],
 })
@@ -27,9 +37,18 @@ const errorLogger = createLogger({
   format: combine(label({ label: 'PH' }), timestamp(), myFormat),
   transports: [
     new transports.Console(),
-    new transports.File({
-      filename: path.join(process.cwd(), 'logs', 'winston', 'success.log'),
-      level: 'error',
+    new DailyRotateFile({
+      filename: path.join(
+        process.cwd(),
+        'logs',
+        'winston',
+        'errors',
+        'UM-%DATE%-error.log.log'
+      ),
+      datePattern: 'DD-MM-YYYY-HH',
+      zippedArchive: true,
+      maxSize: '20m',
+      maxFiles: '14d',
     }),
   ],
 })
